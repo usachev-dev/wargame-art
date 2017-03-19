d890d819128049dc4b367322d5b8344fa245c946
Alexey Usachev <usachev.dev@gmail.com>
2017-03-19 18:39:10 +0300

    homepage progress

--- public/javascripts/angular/entryCtrl.js
+++ public/javascripts/angular/entryCtrl.js
@@ -1,7 +1,22 @@
 app.controller("entryCtrl",['entryData','$scope','$location',entryCtrl]);
 app.service('entryData',['$http', entryData]);
+app.directive('imageonload', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('load', function() {
+                imageLoaded();
+            });
+            element.bind('error', function(){
+                console.log('image could not be loaded');
+            });
+        }
+    };
+});
 
-function entryData($http){
+
+
+    function entryData($http){
     this.get = function(gallerySlug) {
         return $http.get('/api/gallery/'+gallerySlug);
     }
@@ -13,7 +28,7 @@ function entryCtrl(entryData,$scope,$location){
     this.entry = {};
     var imageStoragePath = 'https://s3.amazonaws.com/wargame-art/gallery/'
     this.modal = {
-        state:'open',
+        //state:'open',
         image:''
     };
     absUrl = $location.absUrl();
@@ -24,22 +39,12 @@ function entryCtrl(entryData,$scope,$location){
             function(data){
                 self.entry = data;
                 self.modal.image = imageStoragePath+data.slug+'/'+data.mainImage.filename;
-                $scope.$on('$viewContentLoaded', function(event) {
-                    console.log("onload launched");
-                });
-                angular.element(function () {
-                    console.log('page loading completed');
-                });
-
-                window.onload = function() {
-                    console.log("onload launched");
-                    var images = [];
-                    for (i in self.entry.images) {
-                        images[i] = new Image();
-                        images[i].src = imageStoragePath + gallerySlug + '/' + self.entry.images[i].filename;
-                    }
-                };
-                //console.log(data);
+                self.fullImages = [];
+                self.fullImages.push(imageStoragePath+data.slug+'/'+data.mainImage.filename);
+                for(i in self.entry.images){
+                    self.fullImages.push(imageStoragePath+data.slug+'/'+self.entry.images[i].filename);
+                }
+                //console.log(self.fullImages);
             }
         )
         .error(
@@ -47,6 +52,19 @@ function entryCtrl(entryData,$scope,$location){
                 console.log(e);
             }
         );
+    this.loadedThumbs = 0;
+    this.imageLoaded = function(){
+        self.loadedThumbs++;
+        if (self.loadedThumbs == self.entry.images.length+1){
+            //console.log('all images loaded');
+            var images = [];
+            for (i in self.entry.images) {
+                images[i] = new Image();
+                images[i].src = imageStoragePath + gallerySlug + '/' + self.entry.images[i].filename;
+                //console.log('image preloaded');
+            }
+        }
+    };
 
     this.openImageModal = function(item){
         //self.modal.state = "open";
@@ -60,6 +78,24 @@ function entryCtrl(entryData,$scope,$location){
         $('body').removeClass("no-overflow");
         //console.log("clicked cross");
         $('#image-modal').addClass("closed");
-    }
+    };
 
+    this.nextImageInModal = function(){
+        var n = self.fullImages.indexOf(self.modal.image);
+        if (self.fullImages.length<=n+1){
+            n=0;
+        } else {
+            n++
+        }
+        self.modal.image = self.fullImages[n];
+    };
+    this.prevImageInModal = function(){
+        var n = self.fullImages.indexOf(self.modal.image);
+        if (n<=0){
+            n=self.fullImages.length-1;
+        } else {
+            n--
+        }
+        self.modal.image = self.fullImages[n];
+    };
 };
