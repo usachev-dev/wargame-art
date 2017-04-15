3de394cfed8ee92ebf071a34752c0b7e61ae30a6
Alexey Usachev <usachev.dev@gmail.com>
2017-04-15 09:34:14 +0300

    try to fix favicon

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
 require('./app_api/models/db');
 
 var routes = require('./app_server/routes/index');
